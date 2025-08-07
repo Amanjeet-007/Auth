3164e461491b882a92c9d7c100f55ffa639589d0
Amanjeet kumar <146502426+Amanjeet-007@users.noreply.github.com>
2025-08-07 10:02:07 +0530

    Update connectDb.js
    database name and url setup info

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const DatabaseName = "Auth"
+const DatabaseName = "Auth" //change the name as your data base name in local env & if you have cloud url so remove this
 
 const connectDb = async ()=>{
     try{    
-        await mongoose.connect(`${process.env.MONGODB_KEY}/${DatabaseName}`)
+        await mongoose.connect(`${process.env.MONGODB_KEY}/${DatabaseName}`) // mongodb url 
         console.log(":Database is connected.👍")
     }catch(err){
         console.log("Database Connection error", err)
